5df6dd0fba40c6a303056a0ca296a007bb8aef5d
Chammika Udalagama <chammika@nus.edu.sg>
2023-12-12 00:10:46 +0800

    Added a CCanvasQuiz class to encapsulate quiz operations.

--- canvas.py
+++ canvas.py
@@ -3,6 +3,33 @@ from comms import CComm
 from api import CAPI
 
 
+class CCanvasQuiz:
+    def __init__(self, quiz_id, api_token) -> None:
+        self.comm = CComm(parent_name=f'CCanvasQuiz({quiz_id})')
+        self.api = CAPI(api_token=api_token, target_type=CAPI.TARGET_QUIZZES)
+
+        self.my_quiz_id = quiz_id
+
+        # Add items to API action list
+        self.api.actions[CAPI.ADD_QUESTIONS_TO_QUIZ] = f'{self.my_quiz_id}/questions'
+
+        msg = f"New quiz with id {quiz_id} successfully created."
+        self.comm.print(msg)
+
+        @staticmethod
+        def create_question_list_for_canvas(df_question_bank, quiz_id):
+            mask = df_question_bank['my_quiz_id'] == quiz_id
+            columns_to_keep = ['question_text', 'correct_comments',
+                               'answers', 'question_type', 'points_possible']
+            question_info_dict = df_question_bank.loc[mask,
+                                                      columns_to_keep].T.to_dict()
+
+            question_info = [{'question': question_info_dict[key]}
+                             for key in question_info_dict.keys()]
+
+            return question_info
+
+
 class CCanvas:
     def __init__(self, api_token, course_id=None) -> None:
         self.comm = CComm(parent_name='CCanvas')
@@ -48,6 +75,16 @@ class CCanvas:
         # df = CCanvas.response_to_dataframe(response)
 
         if response is not None:
+            new_quiz_id = response.json()['id']
+            return CCanvasQuiz(
+                quiz_id=new_quiz_id,
+                api_token=self.api.api_token)
+
+    def add_quesitions_ti_quiz(self, quiz_id, question_info):
+        response = self.api.do(CAPI.CREATE_NEW_QUIZ, json_data=quiz_info)
+        # df = CCanvas.response_to_dataframe(response)
+
+        if response is not None:
             msg = f"New quiz with id {response.json()['id']} successfully created."
             self.comm.print(msg)
 
@@ -72,16 +109,3 @@ class CCanvas:
             return None
         else:
             return pd.DataFrame(response.json())
-
-    @staticmethod
-    def create_question_list_for_canvas(df_question_bank, quiz_id):
-        mask = df_question_bank['my_quiz_id'] == quiz_id
-        columns_to_keep = ['question_text', 'correct_comments',
-                           'answers', 'question_type', 'points_possible']
-        question_info_dict = df_question_bank.loc[mask,
-                                                  columns_to_keep].T.to_dict()
-
-        question_info = [{'question': question_info_dict[key]}
-                         for key in question_info_dict.keys()]
-
-        return question_info
