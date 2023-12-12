668efce53312c21191ee44844fcddb8088083b0e
Chammika Udalagama <chammika@nus.edu.sg>
2023-12-12 22:15:34 +0800

    Updated by replacing `applymap()` with map()

--- convert_excel.py
+++ convert_excel.py
@@ -7,31 +7,31 @@ def extract_quiz_info(filename):
     df = pd.read_excel(filename, sheet_name='Quiz Info')
     df.columns = [column.lower().replace(' ', '_') for column in df.columns]
     time_columns = ['unlock_at', 'due_at', 'show_correct_answers_at']
-    df[time_columns] = df[time_columns].applymap(datetime.isoformat)
+    df[time_columns] = df[time_columns].map(datetime.isoformat)
 
     df['time_limit'] = df['time_limit'].apply(
         lambda limit: None if limit == 0 else limit)
 
-    quiz_ids = df['my_quiz_id'].unique().tolist()
+    quiz_ids = df['my_quiz_label'].unique().tolist()
     print(f'I found the following {len(quiz_ids)} unique quiz ids:')
     for index, quiz_id in enumerate(quiz_ids, start=1):
         print(index, ':', quiz_id)
 
-    df.set_index('my_quiz_id', inplace=True)
+    # df.set_index('my_quiz_label', inplace=True)
 
     return df
 
 
 def expand_question_bank(df):
-    df['my_quiz_id'] = df['my_quiz_id'].str.split(',')
+    df['my_quiz_label'] = df['my_quiz_label'].str.split(',')
 
     df_expanded = pd.DataFrame(columns=df.columns)
 
     for index, row in df.iterrows():
-        for quiz_id in row['my_quiz_id']:
+        for quiz_id in row['my_quiz_label']:
             new_index = len(df_expanded)
             df_expanded.loc[new_index] = row
-            df_expanded.loc[new_index, 'my_quiz_id'] = quiz_id
+            df_expanded.loc[new_index, 'my_quiz_label'] = quiz_id
 
     return df_expanded
 
@@ -48,7 +48,7 @@ def extract_question_bank(filename):
     answer_columns = ['a', 'b', 'c', 'd', 'e']
     df['a'] = df['a'].apply(lambda cell: {'text': cell, 'weight': 100})
 
-    df[answer_columns[1:]] = df[answer_columns[1:]].applymap(
+    df[answer_columns[1:]] = df[answer_columns[1:]].map(
         lambda cell: {'text': cell, 'weight': 0}
     )
 
